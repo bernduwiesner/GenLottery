bb1d620d35927c8eb65bd02e80fee9957d4fbcef
Bernd U. Wiesner <32413911+bernduwiesner@users.noreply.github.com>
2019-07-20 14:08:39 +0100

    Add files via upload

--- versioning.py
+++ versioning.py
@@ -1,1 +1,29 @@
-../Imports/versioning.py
+# encoding: utf-8
+#
+#   Copyright (c) 2019 Bernd Wiesner. bernduwiesner@yahoo.co.uk
+#   All rights reserved
+#
+"""Simple version processing
+
+"""
+import dataclasses
+
+
+@dataclasses.dataclass
+class VersionData:
+    """Dataclass for version data
+
+    """
+
+    name: str = ""
+    major: int = 0
+    minor: int = 0
+    patch: int = 0
+
+    @property
+    def version(self) -> str:
+        """A version property
+
+            :return: str a populated version
+        """
+        return f"{self.major}.{self.minor}.{self.patch}"
